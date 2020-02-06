b4ed917709f0e95a38fae1501dbecaad111eeb02
Marcin Sobczyk <msobczyk@redhat.com>
2020-02-06 10:47:46 +0100

    py3: Allow unicode in yaml status output

--- lago/plugins/output.py
+++ lago/plugins/output.py
@@ -106,7 +106,12 @@ class JSONOutFormatPlugin(OutFormatPlugin):
 
 class YAMLOutFormatPlugin(OutFormatPlugin):
     def format(self, info_dict):
-        return yaml.dump(info_dict, default_flow_style=False)
+        return yaml.safe_dump(
+            info_dict,
+            default_flow_style=False,
+            encoding='utf-8',
+            allow_unicode=True
+        )
 
 
 class FlatOutFormatPlugin(OutFormatPlugin):
